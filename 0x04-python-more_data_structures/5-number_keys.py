db3265d101319ca1c5753459d3693ff016395e7d
Neemahj <neemahjinadu@gmail.com>
2022-06-09 14:19:06 -0700

    Task 5 commit

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary
+    Args:
+        a_dictionary: given dictionary
+    Return:
+        length of the key list
+    """
+    return len(a_dictionary.keys())
