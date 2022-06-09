ae955f94fb9c40466b0ed113aacb808ffe0fa13b
Neemahj <neemahjinadu@gmail.com>
2022-06-09 14:47:20 -0700

    Task 11 commit

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    """
-    Returns a key with the biggest integer value.
-    Assume all values are only integers
-    Args:
-        a_dictionary: Given dictionary
-    Return:
-        A key with the biggest integer value.
-    """
-    if not a_dictionary:
-        return None
-    return max(a_dictionary.keys(), key=a_dictionary.get)
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
